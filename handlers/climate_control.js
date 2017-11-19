bd7b6e357f03a04fd109bbbb97cb1deb504eca57
Jonathan Lewis <futbolpal@gmail.com>
2017-11-19 11:05:51 -0500

    Refactoring to extract and leverage User devices configuration

--- handlers/climate_control.js
+++ handlers/climate_control.js
@@ -6,6 +6,8 @@ const Intents     = require('../intents');
 const HubState    = require('../hub_state');
 const HomeActions = require('../home_actions_helper');
 
+const HandlerName = 'climate_control';
+
 Intents.INTENT_CLIMATE_CONTROL_STATUS       = "com.harmony-home.intent.climate-control.temperature.status";
 Intents.INTENT_CLIMATE_CONTROL_UP           = "com.harmony-home.intent.climate-control.temperature.up";
 Intents.INTENT_CLIMATE_CONTROL_DOWN         = "com.harmony-home.intent.climate-control.temperature.down";
@@ -26,25 +28,24 @@ Intents.INTENT_GROUP_CLIMATE_CONTROL = [
   Intents.INTENT_CLIMATE_CONTROL_POWER_ON     
 ];
 
-const Commands = {
-  POWER : "PowerToggle",
-  UP : "Temp+",
-  DOWN : "Temp-"
-}
-
-const handleClimateControl = (hubState, conversationToken, intent, request, reply) => {
-  let device = hubState.deviceByName("Friedrich Climate Control");
+const handleClimateControl = (hubState, context, request, reply) => {
+  let {intent, userId, conversationToken} = context;
   let intentName = intent.intent;
+
+  let user = Users.findBy(userId);
+  let deviceConfiguration = user.deviceByHandler(HandlerName);
+  let device = hubState.deviceByName(deviceConfiguration.name);
+  let commands = deviceConfiguration.commands;
 
   let intentMap = {};
   intentMap[Intents.INTENT_CLIMATE_CONTROL_STATUS]      = { command : null, response: handleReadStatus }   
-  intentMap[Intents.INTENT_CLIMATE_CONTROL_UP]          = { command : Commands.UP, response: "Ok" } 
-  intentMap[Intents.INTENT_CLIMATE_CONTROL_DOWN]        = { command : Commands.DOWN, response: "Ok" }
+  intentMap[Intents.INTENT_CLIMATE_CONTROL_UP]          = { command : commands.UP, response: "Ok" } 
+  intentMap[Intents.INTENT_CLIMATE_CONTROL_DOWN]        = { command : commands.DOWN, response: "Ok" }
   intentMap[Intents.INTENT_CLIMATE_CONTROL_RESET]       = { command : null, response: handleSetStatus }
   intentMap[Intents.INTENT_CLIMATE_CONTROL_SET]         = { command : null, response: handleSetTemperature }
   intentMap[Intents.INTENT_CLIMATE_CONTROL_ADJUST]      = { command : null, response: handleAdjustTemperature }
-  intentMap[Intents.INTENT_CLIMATE_CONTROL_POWER_OFF]   = { command : Commands.POWER, response: handleTogglePower }
-  intentMap[Intents.INTENT_CLIMATE_CONTROL_POWER_ON]    = { command : Commands.POWER, response: handleTogglePower }
+  intentMap[Intents.INTENT_CLIMATE_CONTROL_POWER_OFF]   = { command : commands.POWER, response: handleTogglePower }
+  intentMap[Intents.INTENT_CLIMATE_CONTROL_POWER_ON]    = { command : commands.POWER, response: handleTogglePower }
 
   let command = intentMap[intentName].command;
   let intentResponse = intentMap[intentName].response;
@@ -58,7 +59,7 @@ const handleClimateControl = (hubState, conversationToken, intent, request, repl
   }
   commandPromise.then((response) => {
     if(typeof(intentResponse) === 'function'){
-      return intentResponse(hubState, conversationToken, intent, request, reply, device);
+      return intentResponse(hubState, conversationToken, intent, request, reply, device, deviceConfiguration);
     } else {
       return reply.json(HomeActions.createSimpleReply(conversationToken, intentResponse));
     }
@@ -76,8 +77,8 @@ const queryContains = (intent, needle) => {
   return intent.raw_inputs.find((raw_input) => raw_input.query.includes(needle)) != undefined;
 }
 
-const adjustTemperature = (hubState, device, amount, delay = 750) => {
-  let command = amount > 0 ? Commands.UP : Commands.DOWN;
+const adjustTemperature = (hubState, device, deviceConfig, amount, delay = 750) => {
+  let command = amount > 0 ? deviceConfig.commands.UP : deviceConfig.commands.DOWN;
   return HomeActions.repeatCommands(Math.abs(amount), delay, () =>{
     return hubState.executeCommand(true, device.label, command) 
   }).then(() => {;
@@ -88,20 +89,20 @@ const adjustTemperature = (hubState, device, amount, delay = 750) => {
 ///
 // Handler Methods
 //
-const handleSetTemperature = (hubState, conversationToken, intent, request, reply, device) => {
-  reply.json(HomeActions.createSimpleReply(conversationToken, "Ok"));
+const handleSetTemperature = (hubState, conversationToken, intent, request, reply, device, deviceConfig) => {
+  reply(HomeActions.createSimpleReply(conversationToken, "Ok"));
   let target_temp = argsTemperature(intent);
   let diff = hubState.state.climate_control.temp - target_temp;
-  return adjustTemperature(hubState, device, diff);
+  return adjustTemperature(hubState, device, deviceConfig, diff);
 }
 
-const handleAdjustTemperature = (hubState, conversationToken, intent, request, reply, device) => {
+const handleAdjustTemperature = (hubState, conversationToken, intent, request, reply, device, deviceConfig) => {
   reply.json(HomeActions.createSimpleReply(conversationToken, "Ok"));
   let amount = argsTemperature(intent);
   if(queryContains(intent, "down")){
     amount *= -1;
   }
-  return adjustTemperature(hubState, device, amount);
+  return adjustTemperature(hubState, device, deviceConfig, amount);
 }
 
 const handleTogglePower = (hubState, conversationToken, intent, request, reply, device) => {
--- handlers/tv_control.js
+++ handlers/tv_control.js
@@ -5,6 +5,9 @@ const Q = require('q');
 const Intents     = require('../intents');
 const HubState    = require('../hub_state');
 const HomeActions = require('../home_actions_helper');
+const Users       = require('../models/user');
+
+const HandlerName = 'tv_control';
 
 Intents.INTENT_TV_CONTROL_POWER_OFF    = "com.harmony-home.intent.tv.off";
 Intents.INTENT_TV_CONTROL_POWER_ON     = "com.harmony-home.intent.tv.on";
@@ -20,15 +23,21 @@ Intents.INTENT_GROUP_TV_CONTROL = [
   Intents.INTENT_TV_CONTROL_MUTE         ,
 ];
 
-const handleTvControl = (hubState, conversationToken, intent, request, reply) => {
+const handleTvControl = (hubState, context, request, reply) => {
+  let {intent, userId, conversationToken} = context;
   let intentName = intent.intent;
-  let device = hubState.deviceByName("TV");
+
+  let user = Users.findBy(userId);
+  let deviceConfiguration = user.deviceByHandler(HandlerName);
+  let device = hubState.deviceByName(deviceConfiguration.name);
+  let commands = deviceConfiguration.commands;
+
   let intentMap = {};
-  intentMap[Intents.INTENT_TV_CONTROL_POWER_OFF] = { command : "PowerOff", response: "TV is now off" };
-  intentMap[Intents.INTENT_TV_CONTROL_POWER_ON] = { command : "PowerOn", response: "TV is now on" };
-  intentMap[Intents.INTENT_TV_CONTROL_VOLUME_UP] =  { command : "VolumeUp", response: "Ok" };
-  intentMap[Intents.INTENT_TV_CONTROL_VOLUME_DOWN] = { command : "VolumeDown", response: "Ok" };
-  intentMap[Intents.INTENT_TV_CONTROL_MUTE] = { command : "Mute", response: "Ok" };
+  intentMap[Intents.INTENT_TV_CONTROL_POWER_OFF] = { command : commands.POWER_OFF, response: "Ok" };
+  intentMap[Intents.INTENT_TV_CONTROL_POWER_ON] = { command : commands.POWER_ON, response: "Ok" };
+  intentMap[Intents.INTENT_TV_CONTROL_VOLUME_UP] =  { command : commands.VOLUME_UP, response: "Ok" };
+  intentMap[Intents.INTENT_TV_CONTROL_VOLUME_DOWN] = { command : commands.VOLUME_DOWN, response: "Ok" };
+  intentMap[Intents.INTENT_TV_CONTROL_MUTE] = { command : commands.MUTE, response: "Ok" };
 
   let command = intentMap[intentName].command;
   let intentResponse = intentMap[intentName].response;
