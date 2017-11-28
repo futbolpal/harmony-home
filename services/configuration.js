ed400338e620fde0405c684704e47167675bec9e
Jonathan Lewis <futbolpal@gmail.com>
2017-11-28 10:57:16 -0500

    Allow users to configure through browser flow

--- services/configuration.js
+++ services/configuration.js
@@ -1,6 +1,8 @@
 'use strict';
 
 const util = require('util');
+const { ActionsSdkApp } = require('actions-on-google');
+
 const User = require('../models/user');
 const OAuth = require('../services/oauth');
 
@@ -8,13 +10,18 @@ const Configuration = {}
 
 const askForSignIn = (request, reply) => {
   const app = new ActionsSdkApp({request: request, response: reply});
-  return app.askForSignIn();
+  return app.isRequestFromGoogle(process.env.PROJECT_ID).then(() => {
+    return app.askForSignIn();
+  }).catch(() => {
+    let redirect = util.format('/auth?redirect_uri=%s', '/configuration');
+    return reply.redirect(redirect);
+  });
 }
 
 Configuration.register = (server) => {
   server.get('/configuration', function(request, reply) {
     console.log("GET /configuration");
-    let accessToken = request.query.accessToken
+    let accessToken = request.query.accessToken || request.query.code;
     const withUser = (tokenData) => {
       User.find_or_create(tokenData.uid).then((user) => {
         return reply.render('configuration', {
@@ -24,8 +31,8 @@ Configuration.register = (server) => {
         });
       });
     }
-    const withoutUser = () => { askForSignIn(request, reply); }
-    OAuth.retrieveAuth(accessToken).then(withUser, withoutUser);
+    const withoutUser = () => { return askForSignIn(request, reply); }
+    return OAuth.retrieveAuth(accessToken).then(withUser, withoutUser);
   });
 
   server.post('/configuration', function(request, reply) {
