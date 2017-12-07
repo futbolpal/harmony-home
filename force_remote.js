6075f33d6e28995f1ae63b6ea076b639b951ab06
Jonathan Lewis <futbolpal@gmail.com>
2017-12-07 16:17:09 -0500

    Added test coverage

--- force_remote.js
+++ force_remote.js
@@ -1,25 +1,10 @@
 'use strict';
 
-const HarmonyUtils = require('harmony-hub-util');
-const HubState = require('./hub_state');
+const ForceRemoteUtils = require('./force_remote_utils');
 const RedisClient = require('./redis_client');
-const User = require('./models/user');
-
-const forceDefaultRemote = (user) => {
-  let ip = user.attributes.hubState.ip;
-  HubState.init(ip).then((hub) => {
-    hub.forceDefaultRemote();
-  });
-}
-
-const checkRemoteStatus = function(){
-  User.all().then((users) => { 
-    return users.map(forceDefaultRemote);
-  });
-};
 
 RedisClient.client().on('ready', function(){
-  checkRemoteStatus();
-  setInterval(checkRemoteStatus, 30000);
+  ForceRemoteUtils.checkRemoteStatus();
+  setInterval(ForceRemoteUtils.checkRemoteStatus, 30000);
 });;
 
