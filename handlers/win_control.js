e6b8ae6fccec28e2d1a132a235635a8dfda5ebf6
Jonathan Lewis <futbolpal@gmail.com>
2017-12-10 22:16:44 -0500

    Added test coverage to win_control

--- handlers/win_control.js
+++ handlers/win_control.js
@@ -1,12 +1,14 @@
 'use strict';
 
 const Intents     = require('../intents');
-const HomeActions = require('../home_actions_helper');
+const { createSimpleReply } = require('../home_actions_helper');
 
 const HandlerName = 'win_control';
 
 Intents.INTENT_GROUP_WIN_CONTROL = [
 ];
+
+const intentMap = {};
 
 const handleWinControl = (context, request, reply) => {
   let {hubState, intent, user, conversationToken} = context;
@@ -16,12 +18,17 @@ const handleWinControl = (context, request, reply) => {
   let device = hubState.deviceByName(deviceConfiguration.name);
   let commands = deviceConfiguration.commands;
 
-  let intentMap = {};
+  if(!intentMap[intentName]) { 
+    return reply.json(createSimpleReply(
+          conversationToken, 
+          'Intent could not be handled'
+          ));
+  }
 
   let command = intentMap[intentName].command;
   let intentResponse = intentMap[intentName].response;
   command(device);
-  return reply.json(HomeActions.createSimpleReply(conversationToken, intentResponse));
+  return reply.json(createSimpleReply(conversationToken, intentResponse));
 };
 
 module.exports = handleWinControl;
---
+++ spec/handlers/win_control_spec.js
@@ -0,0 +1,98 @@
+'use strict'
+
+const rewire = require('rewire')
+const sinon = require('sinon')
+const uut = rewire('../../handlers/win_control')
+
+describe('WinControl', function () {
+  let sandbox;
+
+  beforeEach(function(){
+    sandbox = sinon.sandbox.create();
+  });
+
+  afterEach(function(){
+    sandbox.restore();
+  });
+
+  describe("handler name", function(){
+    it("is win_control", function(){
+      expect(uut.__get__("HandlerName")).toEqual("win_control");
+    });
+  });
+
+  describe("intent group", function(){
+    it("is defined", function(){
+      expect(uut.__get__("Intents").INTENT_GROUP_WIN_CONTROL).toBeArray();
+    });
+  });
+  
+  describe("private methods", function(){
+    describe("#handlwWinControl", function(){
+
+      let handleWinControl = uut.__get__("handleWinControl");
+      let createSimpleReplyStub;
+
+      let request, reply;
+      let intent;
+      let user;
+      let hubState;
+      let context;
+      let device;
+      let expectedSpeech;
+
+      beforeEach(function(){
+        createSimpleReplyStub = sandbox.stub();
+        uut.__set__("createSimpleReply", createSimpleReplyStub);
+        reply = { json : sandbox.stub() }
+        request = sandbox.stub();
+        device = {};
+        hubState = { deviceByName: sandbox.stub().returns(device) };
+        user = { deviceByHandler: sandbox.stub().returns(device) };
+        intent = { intent: 'real-intent'};
+        context = () => { return { intent, user, hubState } }; 
+      });
+
+      afterEach(function(){
+        handleWinControl(context(), request, reply); 
+        expect(
+            createSimpleReplyStub.withArgs(
+              sinon.match.any, expectedSpeech
+              ).calledOnce
+            ).toBeTrue();
+      });
+      
+      describe("when intent is found", function(){
+        let intentsMock, intentsMap;
+
+        beforeEach(function(){
+          intentsMock = {};
+          intentsMock.INTENT_WIN_CONTROL_TEST    = intent.intent;
+          intentsMock.INTENT_GROUP_WIN_CONTROL = [ intentsMock.INTENT_WIN_CONTROL_TEST ];
+
+          intentsMap = {};
+          intentsMap[intentsMock.INTENT_WIN_CONTROL_TEST] = { 
+            command: sandbox.stub(), 
+            response: 'Hi' 
+          };
+          uut.__set__("Intents", intentsMock);
+          uut.__set__("intentMap", intentsMap);
+        });
+
+        it("responds wth intent map response", function(){
+          expectedSpeech = 'Hi';
+        });
+      });
+
+      describe("when intent not found", function(){
+        beforeEach(function(){
+          intent = { intent: 'not-found'};
+        });
+
+        it("responds with 'Intent could not be handled'", function(){
+          expectedSpeech = 'Intent could not be handled';
+        });
+      });
+    });
+  });
+})
